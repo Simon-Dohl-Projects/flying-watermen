7da699be957ccb2bacff156935045af439808315
Kyotoo-chan <132380230+Kyotoo-chan@users.noreply.github.com>
2024-03-02 21:15:40 +0100

    added an ui-overlay to signal that you can exit your current chemical state. i also changed every button prompt (#139)
    Co-authored-by: Kyotoo-chan <ben.tischberger@gmail.de>

--- game/entities/objects/stone-tablet/StoneTabletObject.gd
+++ game/entities/objects/stone-tablet/StoneTabletObject.gd
@@ -2,11 +2,10 @@ extends Interactable
 
 signal read(reader: Node2D)
 
-@onready var fill_label: Panel = $FillLabel
 @onready var read_stone_tablet_scene = get_parent().get_node("StoneTabletOverlay")
 
 func _ready():
-	interact_hint = $FillLabel
+	interact_hint = $EKeyDark
 	$AnimationPlayer.play("hover")
 
 func read_stone_tablet(reader: Node):
