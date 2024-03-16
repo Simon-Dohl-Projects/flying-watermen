f44a5cf04a650ad15292376d471ceb72a68e2ea6
Simon Döhl <95179191+TheDying0fLight@users.noreply.github.com>
2024-03-16 19:59:03 +0100

    Fixes (#147)
    * prompt hover fix
    
    * better stone tablets
    
    * improve loading screen
    
    * remove old comment
    
    * Fixed visibility
    
    ---------
    
    Co-authored-by: MettBrotDev <florentin@familiedoll.net>

--- game/globals/Globals.gd
+++ game/globals/Globals.gd
@@ -7,10 +7,9 @@ var main_menu = "res://scenes/ui/menus/mainMenu/mainMenu.tscn"
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("str+1"):
-		# get_tree().change_scene_to_file.call_deferred(main_scene)
 		load_with_loading_screen(main_scene)
 	if event.is_action_pressed("str+2"):
-		get_tree().change_scene_to_file.call_deferred(test_scene)
+		load_with_loading_screen(test_scene)
 	if event.is_action_pressed("str+r"):
 		get_tree().reload_current_scene()
 
--- game/scenes/loading/LoadingScreen.gd
+++ game/scenes/loading/LoadingScreen.gd
@@ -11,7 +11,7 @@ var highest_progress: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	ResourceLoader.load_threaded_request(scene_to_load)
+	ResourceLoader.load_threaded_request(scene_to_load, "", true)
 	animation.play("default")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
