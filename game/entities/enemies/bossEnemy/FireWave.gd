e444b6e5d7a53f10c69f8c61c42eed3c43827247
JuliaLujlia <juliateddy5@gmail.com>
2024-03-17 19:45:56 +0100

    FireBoss Asset

--- game/entities/enemies/bossEnemy/FireWave.gd
+++ game/entities/enemies/bossEnemy/FireWave.gd
@@ -41,7 +41,7 @@ func deal_dmg():
 	var player_distance: int = (player.global_position - self.global_position).length()
 	if inner_radius <= player_distance && player_distance <= outer_radius:
 		var health_component = player.get_node("HealthComponent")
-		if health_component.can_take_damage:
+aa		if health_component.can_take_damage:
 			health_component.take_damage(damage, Element.Type.Fire)
 			player.get_node("HeatComponent").increase_heat(75)
 			can_damage = false
