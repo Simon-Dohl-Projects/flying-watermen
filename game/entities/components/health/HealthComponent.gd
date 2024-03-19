56f12a78bb777a0b59b2bd9d2db3afdf2772abd7
Marcel-John <116278602+Marcel-John@users.noreply.github.com>
2024-03-19 14:43:59 +0100

    fixed damage over time not killing (#149)

--- game/entities/components/health/HealthComponent.gd
+++ game/entities/components/health/HealthComponent.gd
@@ -60,6 +60,7 @@ func take_damage_overtime(amount: int, damage_type: Element.Type, time: int):
 	while can_take_damage_over_time >= 0:
 		can_take_damage_over_time -= 1
 		health -= 1
+		health_changed.emit(health, -1)
 		await get_tree().create_timer(0.3).timeout
 
 func heal(amount: int):
