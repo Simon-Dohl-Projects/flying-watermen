c9c186ad01548dac2eb18187e43534ebd4bb745f
Simon Döhl <simon.doehl@googlemail.com>
2024-03-20 22:34:35 +0100

    waterprojectile

--- game/entities/projectiles/Projectile.gd
+++ game/entities/projectiles/Projectile.gd
@@ -12,7 +12,6 @@ class_name Projectile extends RigidBody2D
 @onready var hitbox: Area2D = $Area2D
 @onready var hitbox_shape: CollisionShape2D = $Area2D/CollisionShape2D
 
-var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 var velocity_offset: Vector2 = Vector2.ZERO
 var direction: Vector2 = Vector2.ZERO
 var start_parent: Node = get_parent()
@@ -23,10 +22,15 @@ func _ready():
 	if collision_shape:
 		hitbox_shape.set_shape(collision_shape.shape)
 		collision_shape.queue_free()
-	if sprite: $Sprite2D.queue_free()
+	if sprite:
+		$Sprite2D.queue_free()
+		sprite.reparent.call_deferred(self)
 	set_linear_velocity((direction * speed) + velocity_offset)
 	await get_tree().create_timer(life_time_seconds).timeout
 	queue_free()
+
+func _integrate_forces(state):
+	rotation = linear_velocity.angle()
 
 func _on_area_2d_body_entered(body):
 	var health_component: HealthComponent = body.get_node_or_null("HealthComponent")
